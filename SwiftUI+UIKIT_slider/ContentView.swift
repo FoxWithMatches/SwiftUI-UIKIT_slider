54c4be6aa5cab120a447d2d29dd75a04680b786b
Alisa Ts <freiheiit92@mail.ru>
2022-01-31 18:30:22 +0300

    Initial Commit

---
+++ SwiftUI+UIKIT_slider/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI+UIKIT_slider
+//
+//  Created by Alisa Ts on 31.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI+UIKIT_slider/SwiftUI_UIKIT_sliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_UIKIT_sliderApp.swift
+//  SwiftUI+UIKIT_slider
+//
+//  Created by Alisa Ts on 31.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_UIKIT_sliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
